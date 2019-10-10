321f1ed449b733b792a3bf5269c117039735c226
Arely Guevara <arelyguevara@Administrators-MacBook-Pro.local>
2019-10-10 15:16:23 -0400

    changed HashRouter

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,11 +6,11 @@ import Food from './Food';
 import Contact from './Contact';
 import Footer from './Footer';
 import {Container} from 'react-bootstrap';
-import {HashRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = ()  => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Container>
       <Navigation/>
         <Route exact path='/' component={Home}/>
@@ -20,7 +20,7 @@ const App = ()  => {
       </Container>
       
         <Footer/>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
